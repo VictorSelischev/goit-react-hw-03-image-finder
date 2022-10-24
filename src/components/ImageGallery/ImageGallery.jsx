ad2f8d34e30c9745707cd2f4b233bfa673990611
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-10-24 18:48:36 +0300

    styles css ImageGalleryItem

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,9 +1,12 @@
 import { Component } from 'react';
 import css from './ImageGallery.module.css';
+import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 
 class ImageGallery extends Component {
   render() {
-    return <ul className={css.gallery}>{/* Набор <li> с изображениями */}</ul>;
+    return <ul className={css.gallery}>
+      <ImageGalleryItem />
+      {/* Набор <li> с изображениями */}</ul>;
   }
 }
 
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,9 +1,10 @@
 import { Component } from 'react';
+import css from './ImageGalleryItem.module.css';
 
 class ImageGalleryItem extends Component {
   render() {
     return (
-      <li class="gallery-item">
+      <li className={css.galleryItem}>
         <img src="" alt="" />
       </li>
     );
