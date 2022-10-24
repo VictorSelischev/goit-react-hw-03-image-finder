66f696c022962355b16a19a6c1896ffb6907c4ca
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-10-24 17:05:10 +0300

    Create ImageGallery

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class ImageGallery extends Component {
+  render() {
+    return <ul class="gallery">{/* Набор <li> с изображениями */}</ul>;
+  }
+}
+
+export { ImageGallery };
